7834bc7f02f8189ebf04c1a6eba5c55488586968
Dagi2 <56186708+Dagi2@users.noreply.github.com>
2020-01-08 23:34:23 +0300

    delet commit 1st
    hove not done my php parts guys ... tinish tebkugn

---
+++ delete_food_items.php
@@ -0,0 +1,117 @@
+<?php
+include('session_m.php');
+
+if(!isset($login_session)){
+header('Location: managerlogin.php'); 
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>MY KITCHEN</title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/delete_food_items.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+
+  
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+    
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">MY KITCHEN</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $login_session; ?> </a></li>
+            <li class="active"> <a href="managerlogin.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+
+
+
+<div class="container">
+    <div class="jumbotron">
+     <h1>Hello Manager! </h1>
+     <p>Manage all your restaurant from here</p>
+
+    </div>
+    </div>
+
+<div class="container">
+    <div class="container">
+    	<div class="col">
+    		
+    	</div>
+    </div>
+
+    
+    	<div class="col-xs-3" style="text-align: center;">
+
+    	<div class="list-group">
+    		<a href="myrestaurant.php" class="list-group-item ">My Restaurant</a>
+    		<a href="view_food_items.php" class="list-group-item ">View Food Items</a>
+    		<a href="add_food_items.php" class="list-group-item ">Add Food Items</a>
+    		<a href="edit_food_items.php" class="list-group-item ">Edit Food Items</a>
+    		<a href="delete_food_items.php" class="list-group-item active">Delete Food Items</a>
+        <a href="view_order_details.php" class="list-group-item ">View Order Details</a>
+    	</div>
+    </div>
+    
+
+
+    
+    <div class="col-xs-9">
+      <div class="form-area" style="padding: 0px 100px 100px 100px;">
+        <form action="delete_food_items1.php" method="POST">
+        <br style="clear: both">
+          <h3 style="margin-bottom: 25px; text-align: center; font-size: 30px;"> DELETE YOUR FOOD ITEMS FROM HERE </h3>
+
+
+
+  <table class="table table-striped">
+    <thead class="thead-dark">
+      <tr>
+        <th> # </th>
+        <th> Food ID </th>
+        <th> Food Name </th>
+        <th> Price </th>
+        <th> Description </th>
+        <th> Restaurant ID </th>
+      </tr>
+    </thead>
+
+        </form>
+
+        
+        </div>
+      
+    </div>
+</div>
+
+  </body>
+</html>
